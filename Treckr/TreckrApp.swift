0a0e0ab6b5f90d045259a7e7cc9beeffb6f5c581
Nathan Patterson <imnathanpatterson@gmail.com>
2023-04-02 16:30:17 -0400

    Initial Commit

---
+++ Treckr/TreckrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreckrApp.swift
+//  Treckr
+//
+//  Created by Nathan Patterson on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TreckrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
